f985269ec8f835a55faeca28367093a7a091d626
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-06-14 15:40:43 -0700

    fixed errs on section 4

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -21,13 +21,13 @@ class BudgetList:
         return len(self.expenses) + len(self.overages)
 
     def __iter__(self):
-        iter(self.expenses)
+        self.iter_e = iter(self.expenses)
         self.iter_o = iter(self.overages)
         return self
 
     def __next__(self):
         try:
-            return self.iter_o.__next__()
+            return self.iter_e.__next__()
         except StopIteration as stop:
             return self.iter_o.__next__()
             
@@ -46,7 +46,7 @@ def main():
     fig, ax = plt.subplots()
 
     labels = ['Expenses', 'Overages', 'Budget']
-    values = ['myBudgetList.sum_expenses', 'myBudgetList.sum_overages', 'myBudgetList.budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
 
     ax.bar(labels, values, color=['green','red','blue'])
     ax.set_title('Your total expenses vs. total budget')
