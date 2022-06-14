7d4b65809d613ee2cb1daba311a0136c38ab2018
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-06-14 14:04:28 -0700

    test11

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -18,10 +18,18 @@ class BudgetList:
 
     def __len__(self):
         return len(self.expenses) + len(self.overages)
+
     def __iter__(self):
         iter(self.expenses)
-        self.iter_0 = iter(self.overages)
+        self.iter_o = iter(self.overages)
         return self
+
+    def __next__(self):
+        try:
+            return self.iter_o.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+            
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -31,6 +39,9 @@ def main():
         myBudgetList.append(expense.amount)
 
     print('The count of all expenses: ', str(len(myBudgetList)))
+
+    for entry in myBudgetList:
+        print(entry)
 
 if __name__ == "__main__":
     main()
